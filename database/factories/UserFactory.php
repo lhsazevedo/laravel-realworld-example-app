12040d9a3b30fa363e424a38c678b82492cf0693
Taylor Otwell <taylorotwell@gmail.com>
2017-04-11 10:41:19 -0500

    rename ModelFactory to UserFactory to encourage separate files

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,17 +1,24 @@
 <?php
 
+use App\Article;
+use App\Comment;
+use App\Tag;
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Carbon;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
 |--------------------------------------------------------------------------
 |
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
 |
 */
 
-$factory->define(App\User::class, function (\Faker\Generator $faker) {
+$factory->define(User::class, function (Faker $faker) {
 
     return [
         'username' => str_replace('.', '', $faker->unique()->userName),
@@ -22,7 +29,7 @@ $factory->define(App\User::class, function (\Faker\Generator $faker) {
     ];
 });
 
-$factory->define(App\Article::class, function (\Faker\Generator $faker) {
+$factory->define(Article::class, function (Faker $faker) {
 
     static $reduce = 999;
 
@@ -30,25 +37,25 @@ $factory->define(App\Article::class, function (\Faker\Generator $faker) {
         'title' => $faker->sentence,
         'description' => $faker->sentence(10),
         'body' => $faker->paragraphs($faker->numberBetween(1, 3), true),
-        'created_at' => \Carbon\Carbon::now()->subSeconds($reduce--),
+        'created_at' => Carbon::now()->subSeconds($reduce--),
     ];
 });
 
-$factory->define(App\Comment::class, function (\Faker\Generator $faker) {
+$factory->define(Comment::class, function (Faker $faker) {
 
     static $users;
     static $reduce = 999;
 
-    $users = $users ?: \App\User::all();
+    $users = $users ?: User::all();
 
     return [
         'body' => $faker->paragraph($faker->numberBetween(1, 5)),
         'user_id' => $users->random()->id,
-        'created_at' => \Carbon\Carbon::now()->subSeconds($reduce--),
+        'created_at' => Carbon::now()->subSeconds($reduce--),
     ];
 });
 
-$factory->define(App\Tag::class, function (\Faker\Generator $faker) {
+$factory->define(Tag::class, function (Faker $faker) {
 
     return [
         'name' => $faker->unique()->word,
